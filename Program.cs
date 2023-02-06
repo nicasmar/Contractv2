5f04ea564d593c016a78cc728e4ae49d537a7a8e
Nick Castillo <nickcastillomarin@college.harvard.edu>
2023-02-05 20:02:00 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using Contractv2.Services;
+using Contractv2;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBaFt/QHRqVVhkVFpFdEBBXHxAd1p/VWJYdVt5flBPcDwsT3RfQF5jS35Xd0xiW3xdcHZVRA==;Mgo+DSMBPh8sVXJ0S0J+XE9AflRDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS31TdEVnWHZfcndWRmNaUw==;ORg4AjUWIQA/Gnt2VVhkQlFacldJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxQdkdjXX5XcXZWRWBZUEM=;MTA2NjM2NUAzMjMwMmUzNDJlMzBOb2RwVmZoS0pLL3Fxc1htRXZYdTA5c21sMkdKQlV4WFZtb0duSkF2dU9vPQ==;MTA2NjM2NkAzMjMwMmUzNDJlMzBYYlZKM0R1Yjc3aTFrd256bXkwMlBrM3h1YWIvM0pJNWhLWlhDOXk1RU5BPQ==;NRAiBiAaIQQuGjN/V0Z+WE9EaFtKVmJLYVB3WmpQdldgdVRMZVVbQX9PIiBoS35RdUVhWXtfeXVXRWNdU0N0;MTA2NjM2OEAzMjMwMmUzNDJlMzBHYUdka1Y2Z0dLV2tHOVNEWXd0eVpiSzJ5UnROdjN6cHJRQmlIZVVENWgwPQ==;MTA2NjM2OUAzMjMwMmUzNDJlMzBYZ2RaZEM5aHRkSjFxME5XM0FEUkV6ZDlXOUtZN1ltcTVzOXNHVGxZbDM4PQ==;Mgo+DSMBMAY9C3t2VVhkQlFacldJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxQdkdjXX5XcXZWRWJcUEY=;MTA2NjM3MUAzMjMwMmUzNDJlMzBpa2EwSXVLZ2R1aTlRUjFJOXAxYXI4Qm01LzVKd2FheHQ3WWsxZnBCd25RPQ==;MTA2NjM3MkAzMjMwMmUzNDJlMzBLeERNRjhoWjk4bGZQUU1GcGlRUlFhWHZGL0pKYTZSdFpiRXFObDRtN1ZJPQ==;MTA2NjM3M0AzMjMwMmUzNDJlMzBHYUdka1Y2Z0dLV2tHOVNEWXd0eVpiSzJ5UnROdjN6cHJRQmlIZVVENWgwPQ==");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<WordService>();
+
+builder.Services.AddSyncfusionBlazor();
+
+await builder.Build().RunAsync();
---
+++ Services/WordService.cs
@@ -0,0 +1,64 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+//using Syncfusion.HtmlConverter;
+//using Syncfusion.Pdf;
+using System.IO;
+
+namespace Contractv2.Services
+{
+    public class WordService
+    {
+        public MemoryStream ConvertWordToHtml(string path)
+        {
+            using (FileStream inputStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Open an existing Word document.
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Automatic))
+                {
+                    //Save as a Markdown file into the MemoryStream.
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        document.Save(stream, FormatType.Html);
+                        stream.Position = 0;
+                        return stream;
+                    }
+                }
+            }
+        }
+
+        public MemoryStream ConvertHTMLtoWord(string path)
+        {
+            using (FileStream inputStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Open an existing Word document.
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Automatic))
+                {
+                    //Save as a Markdown file into the MemoryStream.
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        document.Save(stream, FormatType.Docx);
+                        stream.Position = 0;
+                        return stream;
+                    }
+                }
+            }
+        }
+
+        //public MemoryStream ConvertHtmlToPdf(string path)
+        //{
+        //    //Initialize HTML to PDF converter 
+        //    HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+
+        //    //Convert URL to PDF document 
+        //    PdfDocument document = htmlConverter.Convert("https://www.google.com");
+
+        //    MemoryStream stream = new MemoryStream();
+
+        //    //Save and close the PDF document 
+        //    document.Save(stream);
+        //    document.Close(true);
+
+        //    return stream;
+        //}
+    }
+}
